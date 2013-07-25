e88e74207b670af30e6226179a785d70822aedf7
Tim Lovorn <tflovorn@crimson.ua.edu>
2013-07-25 15:13:29 -0500

    working eigendecomposition

--- src/eigen.f90
+++ src/eigen.f90
@@ -9,30 +9,46 @@ contains
         complex*16, dimension(1:N, 1:N), intent(inout) :: H
         integer, intent(in) :: N
         double precision, dimension(1:N), intent(out) :: eigenvals
-        integer :: EigenDecomp, lwork
+        complex*16, dimension(1:1) :: first_work
+        double precision, dimension(1:3*N-2) :: rwork
+        complex*16, dimension(:), allocatable :: work
+        integer :: EigenDecomp, opt_lwork
+        if (N <= 0) then
+            ! TODO - handle invalid argument
+            return
+        end if
+        ! get the optimal value for lwork
+        call ZHEEV('V', 'U', N, H, N, eigenvals, first_work, -1, rwork, EigenDecomp)
+        if (EigenDecomp /= 0) then
+            return
+        end if
+        opt_lwork = first_work(1)
+        ! Make optimal sized work array.
+        ! allocate() crashes on error; TODO handle?
+        ! Can get error code with allocate(work(1:opt_lwork), STAT=code)
+        ! error if code = 0
+        allocate(work(1:opt_lwork)) 
+        ! make the real call to ZHEEV
+        call ZHEEV('V', 'U', N, H, N, eigenvals, work, opt_lwork, rwork, EigenDecomp)
     end function EigenDecomp
-
-    ! Helper function to make the call to ZHEEV - required since we may need
-    ! to adjust the length of work.
-    recursive function EigenDecompHelper(H, N, eigenvals, lwork) result (info)
-        complex*16, dimension(1:N, 1:N), intent(inout) :: H
-        integer, intent(in) :: N
-        double precision, dimension(1:N), intent(out) :: eigenvals
-        integer, intent(in) :: lwork
-        integer :: info
-        double precision, dimension(1:lwork) :: work
-        ! 'V': return eigenvalues and eigenvectors
-        ! 'U': input H should be upper triangular
-        !call ZHEEV('V', 'U', N, H, N, eigenvals, work, lwork, info)
-        ! work(1) contains optimal value of lwork, which is the length of work
-        info = 0
-    end function EigenDecompHelper
-
-    function EigenMnk12()
-        integer :: EigenMnk12, info
-        info = 0
-        if (info /= 0) then
-            ! failure - TODO handle error
+    
+    function EigenMnk12(numLayers, eigenvals, eigenvectors)
+        integer, intent(in) :: numLayers
+        double precision, dimension(1:4*numLayers), intent(out) :: eigenvals
+        complex*16, dimension(1:4*numLayers, 1:4*numLayers), intent(out) :: eigenvectors
+        integer :: EigenMnk12
+        ! initialize Hamiltonian in eigenvectors, since call to EigenDecomp
+        ! will overwrite H with eigenvectors
+        ! TODO - replace temp eigenvectors - assumes numLayers = 1
+        eigenvectors(:, :) = (0.0D0, 0.0D0)
+        eigenvectors(1, 1) = (1.0D0, 0.0D0)
+        eigenvectors(2, 2) = (2.0D0, 0.0D0)
+        eigenvectors(3, 3) = (3.0D0, 0.0D0)
+        eigenvectors(4, 4) = (4.0D0, 0.0D0)
+        print *, eigenvectors
+        EigenMnk12 = EigenDecomp(eigenvectors, 4*numLayers, eigenvals)
+        if (EigenMnk12 /= 0) then
+            ! TODO handle error
         end if
     end function EigenMnk12
 end module eigen
--- src/main.f90
+++ src/main.f90
@@ -1,10 +1,15 @@
 program main
-    use eigen
+    use eigen, only: EigenMnk12
     implicit none
     call runMain()
 contains
     subroutine runMain()
-        integer error
-        error = EigenMnk12()
+        double precision, dimension(1:4) :: eigenvals
+        complex*16, dimension(1:4, 1:4) :: eigenvectors
+        integer :: error
+        error = EigenMnk12(1, eigenvals, eigenvectors)
+        print *, error
+        print *, eigenvals(1), eigenvals(2), eigenvals(3), eigenvals(4)
+        print *, eigenvectors
     end subroutine runMain
 end program main
