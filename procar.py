066554403f9ea8cca544f2d06165ea02a32992ee
Tim Lovorn <tflovorn@crimson.ua.edu>
2013-08-01 14:45:28 -0500

    PROCAR options to reduce memory usage and methods to use 1-based indexes

--- procar.py
+++ procar.py
@@ -21,7 +21,7 @@
 #     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #     THE SOFTWARE.
 #
-import re, collections
+import re
 
 # Represents the data stored in a PROCAR file.
 # Contains properties nonCol, Nk, Nb, Ni, and kPoints.
@@ -30,8 +30,12 @@ class PROCAR(object):
     # Create PROCAR object by reading from the file-like object procarFile.
     # nonCol = true if this is a non-collinear calculation (2 spins: 4 spinor
     # components); otherwise nonCol = false.
-    def __init__(self, procarFile, nonCol):
+    # If lmDecomposed=False, all ion table columns are discared except the
+    # total column. If storeIds=False, entry ids are not stored.
+    def __init__(self, procarFile, nonCol, lmDecomposed=True, storeIds=True):
         self.nonCol = nonCol
+        self.lmDecomposed = lmDecomposed
+        self.storeIds = storeIds
 
         # Start at the beginning of the file
         try:
@@ -54,6 +58,11 @@ class PROCAR(object):
             self.kPoints.append(KPoint(procarFile, self, kId))
             # Advance to the next k-point.
             procarFile.readline() # empty line
+
+    # Return data for the k-point with id given by kId in the PROCAR file
+    # format (the first id is 1, not 0).
+    def kPoint(self, kId):
+        return self.kPoints[kId-1]
    
 # Represents the data for one k-point.
 # Contains properties kId, kx, ky, kz, weight, and bands.
@@ -62,7 +71,8 @@ class KPoint(object):
     # Extract k-point data from procarFile.
     # The next line read should be the first line of the k-point entry.
     def __init__(self, procarFile, procar, kId):
-        self.kId = kId
+        if procar.storeIds:
+            self.kId = kId
         kHead = procarFile.readline()
         # fixed positions since numbers overlap when there is a minus sign
         self.kx, self.ky, self.kz = map(float, [kHead[18:29], kHead[29:40],
@@ -78,6 +88,12 @@ class KPoint(object):
             # Advance to the next band.
             procarFile.readline() # empty line
 
+    # Return data for the band with id given by bandId in the PROCAR file
+    # format (the first id is 1, not 0).
+    def band(self, bandId):
+        return self.bands[bandId-1]
+
+
 # Represents the data for one band belonging to a specific k-point.
 # Contains properties bandId, energy, occ, tables.
 # tables is a list of IonTable objects.
@@ -85,7 +101,8 @@ class Band(object):
     # Extract band data from procarFile.
     # The next line read should be the first line of the band entry.
     def __init__(self, procarFile, procar, bandId):
-        self.bandId = bandId
+        if procar.storeIds:
+            self.bandId = bandId
         bandHead = procarFile.readline().strip().split()
         # energy is the fifth group in the line, isolated by spaces
         self.energy = float(bandHead[4])
@@ -102,38 +119,68 @@ class Band(object):
             # Extract table data.
             self.tables.append(IonTable(procarFile, procar, tableId))
 
+    # Return data for the table with id given by tableId in the PROCAR file
+    # format (the first id is 1, not 0).
+    def table(self, tableId):
+        return self.tables[tableId-1]
+
+
 # Represents a table of ionic data belonging to a (k-point, band) pair.
-# Contains properties tableId, ions, total.
-# ions is a list of Ion objects and total is an Ion object containing the
+# Contains properties tableId, ions, tot.
+# ions is a list of Ion objects and tot is an Ion object containing the
 # total values summed over all ions.
 class IonTable(object):
     # Extract ion table data from procarFile.
     # The next line read should be the first line of the table, containing ion 1
     def __init__(self, procarFile, procar, tableId):
-        self.tableId = tableId
+        if procar.storeIds:
+            self.tableId = tableId
         # read data for each ion
         self.ions = []
         for ionId in range(1, procar.Ni+1):
-            self.ions.append(Ion(procarFile, ionId))
-        # read total
-        self.ions.append(Ion(procarFile, 0))
+            if procar.lmDecomposed:
+                self.ions.append(Ion(procarFile, procar, ionId))
+            else:
+                self.ions.append(IonTotalOnly(procarFile, procar, ionId))
+        # read row containing totals
+        if procar.lmDecomposed:
+            self.tot = Ion(procarFile, procar, 0)
+        else:
+            self.tot = IonTotalOnly(procarFile, procar, 0)
+
+    # Return data for the ion with id given by ionId in the PROCAR file
+    # format (the first id is 1, not 0).
+    def ion(self, ionId):
+        return self.ions[ionId-1]
 
 # Represents data for one ion, belonging to a (k-point, band, ionTable).
 # Contains properties ionId, s, py, pz, px, dxy, dyz, dz2, dxz, dx2, tot.
 class Ion(object):
-    def __init__(self, procarFile, ionId):
-        self.ionId = ionId
+    def __init__(self, procarFile, procar, ionId):
+        if procar.storeIds:
+            self.ionId = ionId
         # each entry in the line is always separated by spaces
         l = procarFile.readline().strip().split()
         self.s, self.py, self.pz, self.px = map(float, [l[1], l[2], l[3], l[4]])
         self.dxy, self.dyz, self.dz2, self.dxz = map(float, [l[5], l[6], l[7], l[8]])
         self.dx2, self.tot = map(float, [l[9], l[10]])
 
+# Represents data for one ion, belonging to a (k-point, band, ionTable).
+# Discards lm-decomposed data. Contains properties ionId, tot.
+class IonTotalOnly(object):
+    def __init__(self, procarFile, procar, ionId):
+        if procar.storeIds:
+            self.ionId = ionId
+        # each entry in the line is always separated by spaces
+        l = procarFile.readline().strip().split()
+        self.tot = float(l[10])
+
 if __name__ == "__main__":
     # test - TODO arguments?
     with open('PROCAR', 'r') as procarFile:
-        procar = PROCAR(procarFile, True)
+        procar = PROCAR(procarFile, True, False, False)
         print(procar.Nk, procar.Nb, procar.Ni)
         # having to index by one less than id's is annoying
         # TODO - is there a simple solution?
-        print(procar.kPoints[0].bands[0].tables[0].ions[19].tot)
+        print(procar.kPoint(1).band(1).table(1).ion(20).tot)
+        raw_input("-->")
